434d97ef0fca2a131ddf05c4932529d2db3d2e12
Fabio Pereira dos Santos <40362225+pereirafabiosantos2018@users.noreply.github.com>
2018-07-09 20:25:21 -0300

    atualização

--- CadastroTarefasRealizadas/VisualizarTarefasRealizadas.Designer.cs
+++ CadastroTarefasRealizadas/VisualizarTarefasRealizadas.Designer.cs
@@ -28,22 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gridTarefasRealizadas = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataCadastro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureFoto = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.atualizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTarefasRealizadas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFoto)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tarefas realizadas";
+            // 
+            // gridTarefasRealizadas
+            // 
+            this.gridTarefasRealizadas.AllowUserToAddRows = false;
+            this.gridTarefasRealizadas.AllowUserToDeleteRows = false;
+            this.gridTarefasRealizadas.AllowUserToResizeColumns = false;
+            this.gridTarefasRealizadas.AllowUserToResizeRows = false;
+            this.gridTarefasRealizadas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTarefasRealizadas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.categoria,
+            this.dataCadastro});
+            this.gridTarefasRealizadas.Location = new System.Drawing.Point(15, 79);
+            this.gridTarefasRealizadas.Name = "gridTarefasRealizadas";
+            this.gridTarefasRealizadas.ReadOnly = true;
+            this.gridTarefasRealizadas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridTarefasRealizadas.Size = new System.Drawing.Size(431, 290);
+            this.gridTarefasRealizadas.TabIndex = 1;
+            this.gridTarefasRealizadas.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridTarefasRealizadas_CellEnter);
+            this.gridTarefasRealizadas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.gridTarefasRealizadas_KeyPress);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 60;
+            // 
+            // categoria
+            // 
+            this.categoria.HeaderText = "CATEGORIA";
+            this.categoria.Name = "categoria";
+            this.categoria.ReadOnly = true;
+            this.categoria.Width = 200;
+            // 
+            // dataCadastro
+            // 
+            this.dataCadastro.HeaderText = "DATA DO CADASTRO";
+            this.dataCadastro.Name = "dataCadastro";
+            this.dataCadastro.ReadOnly = true;
+            this.dataCadastro.Width = 120;
+            // 
+            // pictureFoto
+            // 
+            this.pictureFoto.Location = new System.Drawing.Point(6, 19);
+            this.pictureFoto.Name = "pictureFoto";
+            this.pictureFoto.Size = new System.Drawing.Size(585, 365);
+            this.pictureFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureFoto.TabIndex = 2;
+            this.pictureFoto.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureFoto);
+            this.groupBox1.Location = new System.Drawing.Point(475, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(600, 390);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Foto";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.atualizarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1089, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // atualizarToolStripMenuItem
+            // 
+            this.atualizarToolStripMenuItem.Name = "atualizarToolStripMenuItem";
+            this.atualizarToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.atualizarToolStripMenuItem.Text = "Atualizar";
+            this.atualizarToolStripMenuItem.Click += new System.EventHandler(this.atualizarToolStripMenuItem_Click);
             // 
             // VisualizarTarefasRealizadas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1089, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gridTarefasRealizadas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "VisualizarTarefasRealizadas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Visualizar tarefas realizadas";
+            ((System.ComponentModel.ISupportInitialize)(this.gridTarefasRealizadas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFoto)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView gridTarefasRealizadas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataCadastro;
+        private System.Windows.Forms.PictureBox pictureFoto;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem atualizarToolStripMenuItem;
     }
 }
--- CadastroTarefasRealizadas/VisualizarTarefasRealizadas.cs
+++ CadastroTarefasRealizadas/VisualizarTarefasRealizadas.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CadastroTarefasRealizadas
@@ -15,6 +8,49 @@ namespace CadastroTarefasRealizadas
         public VisualizarTarefasRealizadas()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Lê as tarefas realizadas
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void atualizarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LerTarefasRealizadas();
+        }
+
+        /// <summary>
+        /// Lê as tarefas realizadas
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LerTarefasRealizadas()
+        {
+            
+        }
+
+        /// <summary>
+        /// Exibe os dados completos da tarefa selecionada
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void gridTarefasRealizadas_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Apaga a tarefa selecionada quando a tecla Del for pressionada
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void gridTarefasRealizadas_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char) Keys.Delete)
+            {
+                MessageBox.Show("A tecla Del foi pressionada", "Tarefas realizadas");
+            }
         }
     }
 }
