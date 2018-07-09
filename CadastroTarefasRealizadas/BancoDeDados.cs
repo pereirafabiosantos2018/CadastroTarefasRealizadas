74da479d283edaa1ad7856058ac556b6689926ed
Fabio Pereira dos Santos <40362225+pereirafabiosantos2018@users.noreply.github.com>
2018-07-09 17:44:19 -0300

    Atualização

---
+++ CadastroTarefasRealizadas/BancoDeDados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CadastroTarefasRealizadas
+{
+    public static class BancoDeDados
+    {
+        public static SqlConnection AbrirConexaoBanco()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=COMPUTADOR\\SQLEXPRESS;Integrated Security=True;");
+            
+            try
+            {
+                sqlConnection.Open();
+                MessageBox.Show("Conexão", "A conexão com o banco de dados foi realizada corretamente.");
+                return sqlConnection;
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show("Erro", e1.Message);
+                return null;
+            }
+        }
+    }
+}
--- CadastroTarefasRealizadas/CadastrarTarefa.cs
+++ CadastroTarefasRealizadas/CadastrarTarefa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CadastroTarefasRealizadas
 {
@@ -23,7 +24,7 @@ namespace CadastroTarefasRealizadas
         /// </summary>
         private void ListarCategorias()
         {
-            
+            SqlConnection conexaoBanco = BancoDeDados.AbrirConexaoBanco();
         }
 
         /// <summary>
